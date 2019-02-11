cd699772be7dafdc82300bc16051cd41b080cc2e
ytw <511340079@qq.com>
2019-02-11 18:25:04 +0800

    一致性哈希负载均衡 平滑加权轮询负载均衡

--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalance.java
@@ -33,7 +33,8 @@ import java.util.concurrent.ConcurrentMap;
 
 /**
  * ConsistentHashLoadBalance
- *
+ * 一致性哈希负载均衡
+ * 根据请求的参数进行性哈希运算
  */
 public class ConsistentHashLoadBalance extends AbstractLoadBalance {
 
@@ -44,8 +45,12 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
         String methodName = RpcUtils.getMethodName(invocation);
         String key = invokers.get(0).getUrl().getServiceKey() + "." + methodName;
+        //根据invokers的内存地址生成hashCode
         int identityHashCode = System.identityHashCode(invokers);
+        //根据key从缓存中获取ConsistentHashSelector
         ConsistentHashSelector<T> selector = (ConsistentHashSelector<T>) selectors.get(key);
+        // 如果 invokers 是一个新的 List 对象，意味着服务提供者数量发生了变化，可能新增也可能减少了。
+        // 此时 selector.identityHashCode != identityHashCode 条件成立
         if (selector == null || selector.identityHashCode != identityHashCode) {
             selectors.put(key, new ConsistentHashSelector<T>(invokers, methodName, identityHashCode));
             selector = (ConsistentHashSelector<T>) selectors.get(key);
@@ -55,6 +60,10 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
 
     private static final class ConsistentHashSelector<T> {
 
+
+        /**
+         * 使用 TreeMap 存储 Invoker 虚拟节点
+         */
         private final TreeMap<Long, Invoker<T>> virtualInvokers;
 
         private final int replicaNumber;
@@ -67,18 +76,30 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
             this.virtualInvokers = new TreeMap<Long, Invoker<T>>();
             this.identityHashCode = identityHashCode;
             URL url = invokers.get(0).getUrl();
+            //获取每个invoker的虚拟节点数，默认为160
             this.replicaNumber = url.getMethodParameter(methodName, "hash.nodes", 160);
+            //对哪些参数hash，缺省只对第一个参数 hash
             String[] index = Constants.COMMA_SPLIT_PATTERN.split(url.getMethodParameter(methodName, "hash.arguments", "0"));
             argumentIndex = new int[index.length];
+            //String转为int
             for (int i = 0; i < index.length; i++) {
                 argumentIndex[i] = Integer.parseInt(index[i]);
             }
             for (Invoker<T> invoker : invokers) {
+                //获取服务提供者的地址
                 String address = invoker.getUrl().getAddress();
+                //i < replicaNumber / 4 是因为里面的for循环相当于乘以4
                 for (int i = 0; i < replicaNumber / 4; i++) {
+                    //通过md5将 address + i 转换为16字节长度的摘要
                     byte[] digest = md5(address + i);
+                    //对digest每4个字节计算一次hash，得到四个不同的 long 型正整数，生成4个虚拟节点放入virtualInvokers中，
                     for (int h = 0; h < 4; h++) {
+                        // h = 0 时，取 digest 中下标为 0 ~ 3 的4个字节进行位运算
+                        // h = 1 时，取 digest 中下标为 4 ~ 7 的4个字节进行位运算
+                        // h = 2, h = 3 时过程同上
                         long m = hash(digest, h);
+                        // 将 hash 到 invoker 的映射关系存储到 virtualInvokers 中，
+                        // virtualInvokers 需要提供高效的查询操作，因此选用 TreeMap 作为存储结构
                         virtualInvokers.put(m, invoker);
                     }
                 }
@@ -86,8 +107,12 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         public Invoker<T> select(Invocation invocation) {
+            //将参数转为key
             String key = toKey(invocation.getArguments());
+            //对key进行md5
             byte[] digest = md5(key);
+            // 取 digest 数组的前四个字节进行 hash 运算，再将 hash 值传给 selectForKey 方法，
+            // 寻找合适的 Invoker
             return selectForKey(hash(digest, 0));
         }
 
@@ -102,10 +127,14 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
         }
 
         private Invoker<T> selectForKey(long hash) {
+            // 到 TreeMap 中查找第一个节点值大于或等于当前 hash 的 Invoker
             Map.Entry<Long, Invoker<T>> entry = virtualInvokers.tailMap(hash, true).firstEntry();
+            // 如果 hash 大于 Invoker 在圆环上最大的位置，此时 entry = null，
+            // 需要将 TreeMap 的头结点赋值给 entry
             if (entry == null) {
                 entry = virtualInvokers.firstEntry();
             }
+            //返回invoker
             return entry.getValue();
         }
 
@@ -134,7 +163,6 @@ public class ConsistentHashLoadBalance extends AbstractLoadBalance {
             md5.update(bytes);
             return md5.digest();
         }
-
     }
 
 }
--- dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
+++ dubbo-cluster/src/main/java/com/alibaba/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
@@ -32,47 +32,82 @@ import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Round robin load balance.
- * 
- * Smoothly round robin's implementation @since 2.6.5 
+ * 平滑加权轮询负载均衡
+ * Smoothly round robin's implementation @since 2.6.5
+ *
+ * 每个服务器对应两个权重，分别为 weight 和 currentWeight。
+ * 其中 weight 是固定的，currentWeight 会动态调整，初始值为0。
+ * 当有新的请求进来时，遍历服务器列表，让它的 currentWeight 加上自身权重。
+ * 遍历完成后，找到最大的 currentWeight，并将其减去权重总和，然后返回相应的服务器即可。
  */
 public class RoundRobinLoadBalance extends AbstractLoadBalance {
     public static final String NAME = "roundrobin";
-    
+
     private static int RECYCLE_PERIOD = 60000;
-    
+
     protected static class WeightedRoundRobin {
+        /***
+         * 服务提供者的权重
+         */
         private int weight;
+        /**
+         * 服务提供者当前的权重
+         */
         private AtomicLong current = new AtomicLong(0);
+        /**
+         * 最后一次更新时间
+         */
         private long lastUpdate;
+
         public int getWeight() {
             return weight;
         }
+
         public void setWeight(int weight) {
             this.weight = weight;
+            // 初始情况下，current = 0
             current.set(0);
         }
+
         public long increaseCurrent() {
             return current.addAndGet(weight);
         }
+
         public void sel(int total) {
             current.addAndGet(-1 * total);
         }
+
         public long getLastUpdate() {
             return lastUpdate;
         }
+
         public void setLastUpdate(long lastUpdate) {
             this.lastUpdate = lastUpdate;
         }
     }
 
+    // 嵌套 Map 结构，存储的数据结构示例如下：
+    // {
+    //     "UserService.query": {
+    //         "url1": WeightedRoundRobin@123,
+    //         "url2": WeightedRoundRobin@456,
+    //     },
+    //     "UserService.update": {
+    //         "url1": WeightedRoundRobin@123,
+    //         "url2": WeightedRoundRobin@456,
+    //     }
+    // }
+    // 最外层为服务类名 + 方法名，第二层为 url 到 WeightedRoundRobin 的映射关系。
+    // 这里我们可以将 url 看成是服务提供者的 id
     private ConcurrentMap<String, ConcurrentMap<String, WeightedRoundRobin>> methodWeightMap = new ConcurrentHashMap<String, ConcurrentMap<String, WeightedRoundRobin>>();
+    // 原子更新锁
     private AtomicBoolean updateLock = new AtomicBoolean();
-    
+
     /**
      * get invoker addr list cached for specified invocation
      * <p>
      * <b>for unit test only</b>
-     * 
+     *
      * @param invokers
      * @param invocation
      * @return
@@ -85,59 +120,91 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
         }
         return null;
     }
-    
+
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        //获取key，分组/interface：版本.methodName
         String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
+        // 获取 url 到 WeightedRoundRobin 映射表，如果为空，则创建一个新的
         ConcurrentMap<String, WeightedRoundRobin> map = methodWeightMap.get(key);
         if (map == null) {
             methodWeightMap.putIfAbsent(key, new ConcurrentHashMap<String, WeightedRoundRobin>());
             map = methodWeightMap.get(key);
         }
+        //总权重
         int totalWeight = 0;
         long maxCurrent = Long.MIN_VALUE;
         long now = System.currentTimeMillis();
         Invoker<T> selectedInvoker = null;
         WeightedRoundRobin selectedWRR = null;
+        // 下面这个循环主要做了这样几件事情：
+        //   1. 遍历 Invoker 列表，检测当前 Invoker 是否有
+        //      相应的 WeightedRoundRobin，没有则创建
+        //   2. 检测 Invoker 权重是否发生了变化，若变化了，
+        //      则更新 WeightedRoundRobin 的 weight 字段
+        //   3. 让 current 字段加上自身权重，等价于 current += weight
+        //   4. 设置 lastUpdate 字段，即 lastUpdate = now
+        //   5. 寻找具有最大 current 的 Invoker，以及 Invoker 对应的 WeightedRoundRobin，
+        //      暂存起来，留作后用
+        //   6. 计算权重总和
         for (Invoker<T> invoker : invokers) {
+            //获取invoker的url的识别串
             String identifyString = invoker.getUrl().toIdentityString();
+            //获取weightedRoundRobin
             WeightedRoundRobin weightedRoundRobin = map.get(identifyString);
+            //获取当前invoker的权重
             int weight = getWeight(invoker, invocation);
             if (weight < 0) {
                 weight = 0;
             }
+            // 检测当前 Invoker 是否有对应的 WeightedRoundRobin，没有则创建
             if (weightedRoundRobin == null) {
                 weightedRoundRobin = new WeightedRoundRobin();
+                // 设置 Invoker 权重
                 weightedRoundRobin.setWeight(weight);
+                // 存储 url 唯一标识 identifyString 到 weightedRoundRobin 的映射关系
                 map.putIfAbsent(identifyString, weightedRoundRobin);
                 weightedRoundRobin = map.get(identifyString);
             }
+            // Invoker 权重不等于 WeightedRoundRobin 中保存的权重，说明权重变化了，此时进行更新
             if (weight != weightedRoundRobin.getWeight()) {
-                //weight changed
                 weightedRoundRobin.setWeight(weight);
             }
+            // 让 current 加上自身权重，等价于 current += weight
             long cur = weightedRoundRobin.increaseCurrent();
+            // 设置 lastUpdate，表示近期更新过
             weightedRoundRobin.setLastUpdate(now);
+            // 找出最大的 current
             if (cur > maxCurrent) {
                 maxCurrent = cur;
+                // 将具有最大 current 权重的 Invoker 赋值给 selectedInvoker
                 selectedInvoker = invoker;
+                // 将 Invoker 对应的 weightedRoundRobin 赋值给 selectedWRR，留作后用
                 selectedWRR = weightedRoundRobin;
             }
+            //计算总权重
             totalWeight += weight;
         }
+        // 对 <identifyString, WeightedRoundRobin> 进行检查，过滤掉长时间未被更新的节点。
+        // 该节点可能挂了，invokers 中不包含该节点，所以该节点的 lastUpdate 长时间无法被更新。
+        // 若未更新时长超过阈值后，就会被移除掉，默认阈值为60秒。
         if (!updateLock.get() && invokers.size() != map.size()) {
             if (updateLock.compareAndSet(false, true)) {
                 try {
                     // copy -> modify -> update reference
+                    //CopyOnWrite
                     ConcurrentMap<String, WeightedRoundRobin> newMap = new ConcurrentHashMap<String, WeightedRoundRobin>();
+                    // 拷贝
                     newMap.putAll(map);
                     Iterator<Entry<String, WeightedRoundRobin>> it = newMap.entrySet().iterator();
+                    // 遍历修改，即移除过期记录
                     while (it.hasNext()) {
                         Entry<String, WeightedRoundRobin> item = it.next();
                         if (now - item.getValue().getLastUpdate() > RECYCLE_PERIOD) {
                             it.remove();
                         }
                     }
+                    // 更新引用
                     methodWeightMap.put(key, newMap);
                 } finally {
                     updateLock.set(false);
@@ -145,7 +212,9 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
             }
         }
         if (selectedInvoker != null) {
+            // 让 current 减去权重总和，等价于 current -= totalWeight
             selectedWRR.sel(totalWeight);
+            // 返回具有最大 current 的 Invoker
             return selectedInvoker;
         }
         // should not happen here
